314eb1443c3fe356e9e41f1daef2fe5392e43233
Mohamed780050 <mohamed39omer@gmail.com>
2025-01-09 00:11:55 +0200

    added the folder structure

---
+++ src/middlewares/logEventsMW.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+const currentDate = new Date();
+function logTheEvent(message = "",filename="Logs.log") {
+  let TheDate = `${currentDate.getUTCDate()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getFullYear()}`;
+  let TheHour = `${currentDate.toLocaleTimeString("en", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+  })}`;
+  let TheDay = `${currentDate.toLocaleDateString("en", { weekday: "long" })}`;
+  let fullInfo = `${TheHour} ${TheDay}\t${TheDate}\t${crypto.randomUUID()}\t${message}\n`;
+  if (!fs.existsSync(path.join(process.cwd(), "Logs"))) {
+    fs.mkdirSync(path.join(process.cwd(), "Logs"));
+  } else {
+    fs.appendFileSync(path.join(process.cwd(), "Logs", filename), fullInfo);
+  }
+}
+export default logTheEvent;
