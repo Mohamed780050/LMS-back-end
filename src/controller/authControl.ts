112af5b036106da20a61af0805a318dd0a98e9ec
Mohamed780050 <mohamed39omer@gmail.com>
2025-01-21 19:17:13 +0200

    finished the studentLogin

--- src/controller/authControl.ts
+++ src/controller/authControl.ts
@@ -14,6 +14,7 @@ async function teacherLogin(req: Request, res: Response) {
       res.cookie("refreshToken", response.data.refreshToken, {
         httpOnly: true,
         secure: true,
+        sameSite: "none",
       });
     }
     res
@@ -29,8 +30,27 @@ async function teacherLogin(req: Request, res: Response) {
 }
 async function studentLogin(req: Request, res: Response) {
   try {
+    const { identifier, password } = req.body;
+    const student = new Auth(identifier, password);
+    const response = await student.studentLogin();
+    if (typeof response.data === "object") {
+      res.set("accessToken", response.data.accessToken);
+      res.cookie("refreshToken", response.data.refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+    }
+    res
+      .status(response.statusCode)
+      .json(
+        typeof response.data === "object"
+          ? { accessToken: response.data.accessToken }
+          : response.data
+      );
   } catch (err) {
     console.log(err);
+    res.status(500).json({ data: "Inter server Error" });
   }
 }
 
