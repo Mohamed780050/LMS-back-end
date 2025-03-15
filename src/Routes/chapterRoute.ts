3f3c44af4a7f376992ff4ca9e12ce667438bcc1c
Mohamed780050 <mohamed39omer@gmail.com>
2025-03-15 21:35:10 +0200

    finished chapter name and description

--- src/Routes/chapterRoute.ts
+++ src/Routes/chapterRoute.ts
@@ -3,5 +3,18 @@ import JWTverifyMW from "../middlewares/JWTverifyMW.js";
 import chapterControl from "../controller/chapterControl.js";
 const router = express.Router();
 router.route("/").post(JWTverifyMW, chapterControl.addNewChapter);
-router.route("/:chapterId").get(JWTverifyMW, chapterControl.getAChapter).delete(JWTverifyMW,chapterControl.deleteChapter);
+router
+  .route("/:chapterId")
+  .get(JWTverifyMW, chapterControl.getAChapter)
+  .delete(JWTverifyMW, chapterControl.deleteChapter);
+router.post(
+  "/:chapterId/ChapterName",
+  JWTverifyMW,
+  chapterControl.UpdateChapterName
+);
+router.post(
+  "/:chapterId/chapterDescription",
+  JWTverifyMW,
+  chapterControl.UpdateChapterDescription
+);
 export default router;
--- src/controller/chapterControl.ts
+++ src/controller/chapterControl.ts
@@ -36,6 +36,10 @@ async function getAChapter(req: Request, res: Response) {
 async function deleteChapter(req: Request, res: Response) {
   try {
     const { userId, chapterId } = req.params;
+    if (!userId || !chapterId) {
+      res.status(400).json({ data: "all fields are required" });
+      return;
+    }
     const myChapter = new Chapter(userId, "");
     const response = await myChapter.deleteChapter(chapterId);
     res.status(response.statusCode).json({ data: response.data });
@@ -44,5 +48,47 @@ async function deleteChapter(req: Request, res: Response) {
     res.status(500).json({ data: "internal server Error" });
   }
 }
+async function UpdateChapterName(req: Request, res: Response) {
+  try {
+    const { userId, chapterId } = req.params;
+    const { chapterName } = req.body;
+    if (!userId || !chapterId || !chapterName) {
+      res.status(400).json({ data: "all fields are required" });
+      return;
+    }
+    const myChapter = new Chapter(userId, "");
+    const response = await myChapter.updateChapterName(chapterId, chapterName);
+    res.status(response.statusCode).json({ data: response.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ data: "internal server Error" });
+  }
+}
+async function UpdateChapterDescription(req: Request, res: Response) {
+  try {
+    const { userId, chapterId } = req.params;
+    const { chapterDescription } = req.body;
+    if (!userId || !chapterId || !chapterDescription) {
+      res.status(400).json({ data: "all fields are required" });
+      return;
+    }
+    const myChapter = new Chapter(userId, "");
+    const response = await myChapter.updateChapterDescription(
+      chapterId,
+      chapterDescription
+    );
+    res.status(response.statusCode).json({ data: response.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ data: "internal server Error" });
+  }
+}
 
-export default { addNewChapter, getChapters, getAChapter, deleteChapter };
+export default {
+  addNewChapter,
+  getChapters,
+  getAChapter,
+  deleteChapter,
+  UpdateChapterName,
+  UpdateChapterDescription,
+};
