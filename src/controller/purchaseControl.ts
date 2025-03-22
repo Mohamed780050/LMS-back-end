62e2b4bc552359f5dd64acb3a395b5522dbdff95
Mohamed780050 <mohamed39omer@gmail.com>
2025-03-22 13:27:26 +0200

    finised createAPurchase function

--- src/controller/purchaseControl.ts
+++ src/controller/purchaseControl.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import { Purchase } from "../models/purchaseModel";
 
 async function createAPurchase(req: Request, res: Response) {
   try {
+    const { userId } = req.params;
+    const { courseId } = req.body;
+    const purchase = new Purchase(userId, courseId);
+    const response = await purchase.save();
+    res.status(response.statusCode).json({ data: response.data });
   } catch (err) {
     console.log(err);
     res.status(500).json({ data: "Internal server error" });
